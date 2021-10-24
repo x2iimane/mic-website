1999095fbcf65f325eaa984d7c5b400ea204382e
x2iimane <rafiqimane.mail@gmail.com>
2021-10-24 19:46:15 +0100

    ajout lien langue sur root

--- routes/web.php
+++ routes/web.php
@@ -128,5 +128,7 @@ use Illuminate\Support\Facades\Route;
 
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 //
+
+    Route::get('/lng/{lang}', 'indexController@lng');
 });
 //Route::redirect('/','/en');
