b72dd40899158216ae8a4dadde96fe36c08ad6a4
Félix SAGNOL <gobou0303@gmail.com>
2021-10-26 21:41:29 +0200

    modification des langues

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,13 +33,10 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-<<<<<<< HEAD
             //---------------------------------
             \Illuminate\Session\Middleware\AuthenticateSession::class,
             //------------------------------------------
-=======
             \Illuminate\Session\Middleware\AuthenticateSession::class,
->>>>>>> a732459b0b0cb57dbce55a6666b3a61906bba730
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
